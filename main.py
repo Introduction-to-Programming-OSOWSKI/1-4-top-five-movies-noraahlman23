ec14ba73812bd36740e0fe08c5f13890bf97630e
noraahlman23 <819446@mystma.org>
2021-02-24 16:20:13 +0000

    topFiveMovies

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-#WRITE YOUR CODE IN THIS FILE
+def topFiveMovies(a, b, c, d, e):
+    return (a + b + c + d + e)
+
+print (topFiveMovies("To all the boys i've loved beofre", " Avengerrs: Infinity war", " Guardians of the Galaxy vol.1", " Guardians of the Galaxy vol.2", " The little Mermaid"))
